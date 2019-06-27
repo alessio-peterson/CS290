96f8967b7797a12e2ab9ed7d719ff4863045cd3a
alessio-peterson <52301333+alessio-peterson@users.noreply.github.com>
2019-06-27 16:45:45 -0700

    Add files via upload

---
+++ gitTest1.cpp
@@ -0,0 +1,16 @@
+/********************************************************
+ ** Name: Alessio Peterson
+ ** Date: June 27 2019
+ ** Description: Test file #1 for Github setup
+ *******************************************************/
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "This is a test file for setting up Github" << std::endl;
+  std::cout << "Hello!" << std::endl;
+
+  return 0;
+}
+
---
+++ gitTest2.cpp
@@ -0,0 +1,16 @@
+/**********************************************************
+ ** Name: Alessio Peterson
+ ** Date: June 27 2019
+ ** Description: This is test file #2 for Github setup
+ **********************************************************/
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "This is a test file for setting up Github" << std::endl;
+  std::cout << "Goodbye!" << std::endl;
+
+  return 0;
+}
+
